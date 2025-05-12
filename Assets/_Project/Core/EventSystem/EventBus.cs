3157abdb7d970bc67a796a216a7464c2a150f309
Yunus Emre YALÇINKAYA <yalcinkayayunusemre@gmail.com>
2025-05-12 16:48:34 +0300

    Basic Event System created
    Event system is created. System is designed as indipendent of unity features. Structure supports dependency injection and testing.

---
+++ Assets/_Project/Core/EventSystem/EventBus.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.EventSystem
+{
+    public class EventBus : IEventBus
+    {
+        private readonly Dictionary<Type, List<Delegate>> _subscribers = new();
+
+        public void Subscribe<T>(Action<T> callback)
+        {
+            var type = typeof(T);
+            if (!_subscribers.ContainsKey(type))
+                _subscribers[type] = new List<Delegate>();
+
+            _subscribers[type].Add(callback);
+        }
+
+        public void Unsubscribe<T>(Action<T> callback)
+        {
+            var type = typeof(T);
+            if (_subscribers.TryGetValue(type, out var list))
+                list.Remove(callback);
+        }
+
+        public void Publish<T>(T eventData)
+        {
+            var type = typeof(T);
+            if (_subscribers.TryGetValue(type, out var list))
+            {
+                foreach (var del in list)
+                {
+                    if (del is Action<T> action)
+                        action.Invoke(eventData);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Project/Core/EventSystem/IEventBus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Core.EventSystem
+{
+    public interface IEventBus
+    {
+        void Subscribe<T>(Action<T> callback);
+        void Unsubscribe<T>(Action<T> callback);
+        void Publish<T>(T eventData);
+    }
+}
